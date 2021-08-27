f7450607d7bfd3db91630f0d20eb3cc89256dd67
Denilson 87 <denilsonmucavel@outlook.com>
2021-08-27 16:17:08 -0700

    first push

---
+++ build/precache-manifest.dea7213bc45d418f4e516180621d9b2c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6512600eb386bc1d4a8aea82b2bcefce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c5d8c673768e6f6102c",
+    "url": "/static/css/2.e4777ce8.chunk.css"
+  },
+  {
+    "revision": "bad068c4ad6ad766f08e",
+    "url": "/static/css/main.b66253e5.chunk.css"
+  },
+  {
+    "revision": "4c5d8c673768e6f6102c",
+    "url": "/static/js/2.a044304e.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.a044304e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad068c4ad6ad766f08e",
+    "url": "/static/js/main.2c4abc47.chunk.js"
+  },
+  {
+    "revision": "a18b947d6cb11ecec285",
+    "url": "/static/js/runtime-main.1ae02da6.js"
+  },
+  {
+    "revision": "3f7fc0b6dadbd06e57534706f8b47fe6",
+    "url": "/static/media/backgound.3f7fc0b6.svg"
+  },
+  {
+    "revision": "416dafbe84895ec4a3f01a3b5fdd32f6",
+    "url": "/static/media/logo.416dafbe.svg"
+  }
+]);
